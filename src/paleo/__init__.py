5d2bc3c492dbc8618c9d8755e96ff6bb63a541c7
Ben Pedigo <benjamindpedigo@gmail.com>
2024-10-10 10:13:29 -0700

    sort

--- src/paleo/__init__.py
+++ src/paleo/__init__.py
@@ -10,12 +10,12 @@ from .paleo import (
 )
 
 __all__ = [
-    "get_metaedits",
-    "get_metadata_table",
+    "compare_graphs",
     "get_detailed_change_log",
+    "get_metadata_table",
+    "get_metaedits",
     "get_operation_level2_edit",
     "get_operations_level2_edits",
     "get_root_level2_edits",
-    "compare_graphs",
     "NetworkDelta",
 ]
